8931bed1f2609a49dd81faf97f538fd6dbb136ed
Sergio Torres <sdtorresl@unal.edu.co>
2020-11-24 23:57:02 -0500

    Add opacity transition

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -38,30 +38,34 @@ function slideTo(direction) {
         position: 'absolute',
         top: 0,
         [direction]: 0,
-        width: '100%'
+        width: '100%',
+        opacity: 1
       })
     ], optional),
     query(':enter', [
       style({
-        [direction]: '-100%'
+        [direction]: '-100%',
+        opacity: 0
       })
     ]),
     group([
       query(':leave', [
-        animate('600ms ease', style({
-          [direction]: '100%'
+        animate('700ms ease', style({
+          [direction]: '100%',
+          opacity: 0
         }))
       ], optional),
       query(':enter', [
-        animate('600ms ease', style({
-          [direction]: '0%'
+        animate('700ms ease', style({
+          [direction]: '0%',
+          opacity: 1
         }))
       ])
     ]),
     // Normalize the page style... Might not be necessary
 
     // Required only if you have child animations on the page
-    //query(':leave', animateChild()),
-    //query(':enter', animateChild()),
+    query(':leave', animateChild()),
+    query(':enter', animateChild()),
   ];
 }
